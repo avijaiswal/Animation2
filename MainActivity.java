aef1c594be4eb4e1123efd8ccaacdffbe787365c
avinash jaiswal <jaisavi25@gmail.com>
2016-02-09 13:22:47 +0530

    bran

--- MainActivity.java
+++ MainActivity.java
@@ -20,35 +20,6 @@ private ImageView imageview;
         Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.myanimation);
               imageview.startAnimation(animation);
     }
-    public void clockwise(View view){
-        ImageView image = (ImageView)findViewById(R.id.imageView);
-        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.clockwise);
-        image.startAnimation(animation);
-    }
-
-    public void zoom(View view){
-        ImageView image = (ImageView)findViewById(R.id.imageView);
-        Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.zoom);
-        image.startAnimation(animation1);
-    }
-
-    public void fade(View view){
-        ImageView image = (ImageView)findViewById(R.id.imageView);
-        Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
-        image.startAnimation(animation1);
-    }
-
-    public void blink(View view){
-        ImageView image = (ImageView)findViewById(R.id.imageView);
-        Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.blink);
-        image.startAnimation(animation1);
-    }
-
-    public void move(View view){
-        ImageView image = (ImageView)findViewById(R.id.imageView);
-        Animation animation1 = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.move);
-        image.startAnimation(animation1);
-    }
 
     public void slide(View view){
         ImageView image = (ImageView)findViewById(R.id.imageView);
